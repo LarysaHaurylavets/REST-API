79ea0ac0e83281e2e11866a24afa43a583c98ff4
Larysa Haurylavets <Larysa_Haurylavets@epam.com>
2017-09-01 10:54:03 +0300

    Add test check body is the array of 10 users

--- restAPI.js
+++ restAPI.js
@@ -1,44 +1,49 @@
 'use strict';
 
-let request = require('request');
-let base_url = 'https://jsonplaceholder.typicode.com/users';
-let should = require('chai').should();
+const request = require('request');
+const base_url = 'https://jsonplaceholder.typicode.com/users';
+//const should = require('chai').should();
+var date;
 
 describe('testing rest web-service', function (){
 	
 //we have to add is a done callback. Since Node.js is asynchronous, 
 //the it function will finish before our expect function does.
-//  	it('should returns status code 200', function(done){
-//  		request.get(base_url, function(error,response,body){
-//  			expect(response.statusCode).toBe(200);
-//  			done();
-//  		});
-//  	});
+ 	it('should returns status code 200', function(done){
+ 		request.get(base_url, function(error,response,body){
+ 			expect(response.statusCode).toBe(200);
+ 			done();
+ 		});
+ 	});
 
-// //the content-type header exists in the obtained response
-//  	it('should the content-type header exists', function(done){
-//  		request.get(base_url, function(error,response,body){
-//  			//console.log('Content-Type: '+ JSON.stringify(response.headers));
-//  			expect(JSON.stringify(response.headers)).not.toBe(null);
-//  			done();
-//  		});
+//the content-type header exists in the obtained response
+ 	it('should the content-type header exists', function(done){
+ 		request.get(base_url, function(error,response,body){
+ 			//console.log('Content-Type: '+ JSON.stringify(response.headers));
+ 			expect(JSON.stringify(response.headers)).not.toBe(null);
+ 			done();
+ 		});
  		
-//  	});
+ 	});
 
-// //the value of the content-type header is application/json
-// 	it('should the value of the content-type header is application/json', function(done){
-//  		request.get(base_url, function(error,response,body){
-//  			expect(JSON.stringify(response.headers)).toContain('application/json');
-//  			done();
-//  		});
+//the value of the content-type header is application/json
+	it('should the value of the content-type header is application/json', function(done){
+ 		request.get(base_url, function(error,response,body){
+ 			expect(JSON.stringify(response.headers)).toContain('application/json');
+ 			done();
+ 		});
  		
-//  	});
+ 	});
 
 //the content of the response body is the array of 10 users
- 	it('should the content-type header exists in the obtained response', function(){
- 		request.get(base_url, function(error,response,body){ 			
- 			body.should.have.property('id').with.lengthOf(11);
- 			//done();
+ 	it('should the content-type header exists in the obtained response', function(done){
+ 		request.get(base_url, function(error,response,body){ 
+ 			if(response.statusCode==200){
+ 				date=JSON.parse(body);
+ 				let countUsers=date.length;
+ 				expect(countUsers).toEqual(10);
+ 			};
+ 			done();
  		});
  		
  	});
